aeb3d52d2114609ba7accc5c6c81b7002c138776
lihl <lihl.zb@ikongjian.com>
2023-04-13 13:48:45 +0800

    init

---
+++ src/components/SystemRoleSettings.tsx
@@ -0,0 +1,69 @@
+import { Show } from 'solid-js'
+import IconEnv from './icons/Env'
+import IconX from './icons/X'
+import type { Accessor, Setter } from 'solid-js'
+
+interface Props {
+  canEdit: Accessor<boolean>
+  systemRoleEditing: Accessor<boolean>
+  setSystemRoleEditing: Setter<boolean>
+  currentSystemRoleSettings: Accessor<string>
+  setCurrentSystemRoleSettings: Setter<string>
+}
+
+export default (props: Props) => {
+  let systemInputRef: HTMLTextAreaElement
+
+  const handleButtonClick = () => {
+    props.setCurrentSystemRoleSettings(systemInputRef.value)
+    props.setSystemRoleEditing(false)
+  }
+
+  return (
+    <div class="my-4">
+      <Show when={!props.systemRoleEditing()}>
+        <Show when={props.currentSystemRoleSettings()}>
+          <div>
+            <div class="fi gap-1 op-50 dark:op-60">
+              <Show when={props.canEdit()} fallback={<IconEnv />}>
+                <span onClick={() => props.setCurrentSystemRoleSettings('')} class="sys-edit-btn p-1 rd-50%" > <IconX /> </span>
+              </Show>
+              <span>角色: </span>
+            </div>
+            <div class="mt-1">
+              {props.currentSystemRoleSettings()}
+            </div>
+          </div>
+        </Show>
+        <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="sys-edit-btn">
+            <IconEnv />
+            <span>添加角色预设</span>
+          </span>
+        </Show>
+      </Show>
+      <Show when={props.systemRoleEditing() && props.canEdit()}>
+        <div>
+          <div class="fi gap-1 op-50 dark:op-60">
+            <IconEnv />
+            <span>角色:</span>
+          </div>
+          <p class="my-2 leading-normal text-sm op-50 dark:op-60">指挥助手，设定助手的行为.</p>
+          <div>
+            <textarea
+              ref={systemInputRef!}
+              placeholder="假如你是一个编程高手，帮我回答一下问题...."
+              autocomplete="off"
+              autofocus
+              rows="3"
+              gen-textarea
+            />
+          </div>
+          <button onClick={handleButtonClick} gen-slate-btn>
+            设置
+          </button>
+        </div>
+      </Show>
+    </div>
+  )
+}
